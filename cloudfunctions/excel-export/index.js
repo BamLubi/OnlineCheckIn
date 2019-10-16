bcea5b2ecfacb0911b2cd6b94a5975f537daadbb
BamLubi <1126458447@qq.com>
2019-10-16 16:58:07 +0800

    完善excel导出逻辑

--- cloudfunctions/excel-export/index.js
+++ cloudfunctions/excel-export/index.js
@@ -5,43 +5,52 @@ const path = require('path');
 cloud.init()
 // 云函数入口函数 
 exports.main = async(event, context) => {
-    var tableHead = ["发票编号", "发票代码", "开票时间", "金额"];
-    var tableMap = {
-        styleXmlFile: path.join(__dirname, "styles.xml"),
-        name: Date.now() + "-export",
-        cols: [],
-        rows: [],
-    }
-    //添加表头
-    for (var i = 0; i < tableHead.length; i++) {
-        tableMap.cols[tableMap.cols.length] = {
-            caption: tableHead[i],
-            type: 'string'
+	var tableMap = {
+		styleXmlFile: path.join(__dirname, "styles.xml"),
+		name: Date.now() + "-export",
+		cols: [],
+		rows: [],
+	}
+    if (event.mode == "exchange") {
+        var tableHead = ["昵称", "兑奖次数", "姓名", "电话", "省", "市", "区", "详细地址", "邮编"];
+        //添加表头
+        for (var i = 0; i < tableHead.length; i++) {
+            tableMap.cols[tableMap.cols.length] = {
+                caption: tableHead[i],
+                type: 'string'
+            }
         }
-    }
-    //伪数据
-    const Output = [{
-            发票编号: 0,
-            发票代码: '001',
-            开票时间: '2019-5-1',
-            金额: 100
-        },
-        {
-            发票编号: 1,
-            发票代码: '002',
-            开票时间: '2019-5-1',
-            金额: 200
+        //添加每一行数据
+        for (var i = 0; i < event.list.length; i++) {
+            tableMap.rows[tableMap.rows.length] = [
+                event.list[i].userInfo.nickName,
+				String(event.list[i].exchangeInfo.level),
+                event.list[i].address.userName,
+				String(event.list[i].address.telNumber),
+                event.list[i].address.provinceName,
+                event.list[i].address.cityName,
+                event.list[i].address.countyName,
+                event.list[i].address.detailInfo,
+				String(event.list[i].address.postalCode)
+            ]
         }
-    ]
-    //添加每一行数据
-    for (var i = 0; i < Output.length; i++) {
-        tableMap.rows[tableMap.rows.length] = [
-            Output[i].发票编号,
-            Output[i].发票代码,
-            Output[i].开票时间,
-            Output[i].金额
-        ]
-    }
+	} else if (event.mode == "all"){
+		var tableHead = ["昵称", "签到次数"];
+		//添加表头
+		for (var i = 0; i < tableHead.length; i++) {
+			tableMap.cols[tableMap.cols.length] = {
+				caption: tableHead[i],
+				type: 'string'
+			}
+		}
+		//添加每一行数据
+		for (var i = 0; i < event.list.length; i++) {
+			tableMap.rows[tableMap.rows.length] = [
+				event.list[i].userInfo.nickName,
+				String(event.list[i].signDaysTimes)
+			]
+		}
+	}
     //保存excelResult到相应位置
     var excelResult = nodeExcel.execute(tableMap);
     var filePath = "outputExcels";
